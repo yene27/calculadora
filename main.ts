066872208b6c2a6900cd7bb59ae720817226e631
yene27 <yene27@yahoo.com>
2022-05-30 18:40:13 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,98 @@
+input.onButtonPressed(Button.A, function () {
+    if (cambio == 0) {
+        numero1 += 1
+        basic.showNumber(numero1)
+    }
+    if (cambio == 1) {
+        numero2 += 1
+        basic.showNumber(numero2)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    operador += 1
+    if (operador == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+    } else {
+        if (operador == 2) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+        } else {
+            if (operador == 3) {
+                basic.showLeds(`
+                    # . . . #
+                    . # . # .
+                    . . # . .
+                    . # . # .
+                    # . . . #
+                    `)
+                basic.pause(100)
+                basic.clearScreen()
+            } else {
+                if (operador == 4) {
+                    basic.showLeds(`
+                        . . # . .
+                        . . . . .
+                        # # # # #
+                        . . . . .
+                        . . # . .
+                        `)
+                    basic.pause(100)
+                    basic.clearScreen()
+                } else {
+                    operador = 1
+                    basic.showLeds(`
+                        . . # . .
+                        . . # . .
+                        # # # # #
+                        . . # . .
+                        . . # . .
+                        `)
+                    basic.pause(100)
+                    basic.clearScreen()
+                }
+            }
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    cambio += 1
+    if (cambio == 2) {
+        if (operador == 1) {
+            basic.showNumber(numero1 + numero2)
+        } else {
+            if (operador == 2) {
+                basic.showNumber(numero1 - numero2)
+            } else {
+                if (operador == 3) {
+                    basic.showNumber(numero1 * numero2)
+                } else {
+                    basic.showNumber(numero1 / numero2)
+                }
+            }
+        }
+    }
+})
+let cambio = 0
+let numero2 = 0
+let numero1 = 0
+let operador = 0
+operador = 0
+numero1 = 0
+numero2 = 0
+cambio = 0
